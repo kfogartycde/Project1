8af72787b1469d212dc5aa69f22f66a9d9655e7f
Kevin Fogarty <kfogarty@CDE.com>
2020-01-03 09:43:07 -0500

    first commit

---
+++ kjf.bat
@@ -0,0 +1,14 @@
+curl -X GET \
+  https://api.monday.com/v2/ \
+  -H 'Accept: */*' \
+  -H 'Accept-Encoding: gzip, deflate' \
+  -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjI1MjQ1Njk5LCJ1aWQiOjEwMzQzODUxLCJpYWQiOiIyMDE5LTEwLTI0IDEyOjA3OjMwIFVUQyIsInBlciI6Im1lOndyaXRlIn0.LmkBofY01J9PkdDlEs0ZSUUMRuF_Nk62Pj5Yh39dM5E' \
+  -H 'Cache-Control: no-cache' \
+  -H 'Connection: keep-alive' \
+  -H 'Content-Length: 168' \
+  -H 'Content-Type: application/json' \
+  -H 'Cookie: __cfduid=d580475ff5eb080ca4afa8689679a21e61572616637' \
+  -H 'Host: api.monday.com' \
+  -H 'Postman-Token: 8ea7087d-55f9-4ee8-ad78-873f332b7a08,238287bf-ac5f-4eff-851b-129086908665' \
+  -H 'User-Agent: PostmanRuntime/7.19.0' \
+  -H 'cache-control: no-cache'
